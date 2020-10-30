4b75e7b4ea929c719286c6360e36659568ac8fff
Sohel Mia <sohelcse1999@gmail.com>
2020-10-31 04:13:55 +0600

    phpunit and php_cs fixer install

---
+++ tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Sohel\Skeleton;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_can_be_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
